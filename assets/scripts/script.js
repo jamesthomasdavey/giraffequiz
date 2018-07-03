8862a5f857b93cfee52ce9524ca85b587c24c6c5
James Thomas Davey <jamesthomasdavey@Jamess-MacBook-Pro.local>
2018-07-03 11:07:57 -0700

    fix render of next button after restart

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -86,6 +86,7 @@ const App = (() => {
       quiz.currentIndex = 0;
       progressEl.style.width = `0`;
       instructionEl.innerHTML = `Pick an option below!`;
+      nextButtonEl.style.display = `block`;
       renderAll();
     });
   }
